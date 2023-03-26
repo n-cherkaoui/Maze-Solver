19b1c69a42a4e2d456d615203cb06fc2077d7fad
n-cherkaoui <109431994+n-cherkaoui@users.noreply.github.com>
2023-03-26 16:05:04 -0400

    Update and rename MagicMazeDriver.java to MazeSolverDriver.java

--- MazeSolverDriver.java
+++ MazeSolverDriver.java
@@ -1,9 +1,3 @@
-/** Dr. Andrew Steinberg
- *  COP3503 Computer Science 2
- *  Programming Assignment 2 Driver
- *  Spring 2023
- */
- 
 import java.io.File;
 import java.util.Scanner;
 
@@ -11,11 +5,11 @@ public class MagicMazeDriver
 {   
     public static void main(String args[]) throws Exception
     {
-        MagicMaze maze;
+        MazeSolver maze;
            
         while(true)
         {
-            maze = new MagicMaze(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+            maze = new MazeSolver(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
             maze.solveMagicMaze();
         }
     }
