3e18915837f2f6baf3cb83a8fe6e575ea44aaf89
n-cherkaoui <109431994+n-cherkaoui@users.noreply.github.com>
2023-03-26 16:15:12 -0400

    Update MazeSolverDriver.java

--- MazeSolverDriver.java
+++ MazeSolverDriver.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.util.Scanner;
 
-public class MagicMazeDriver 
+public class MazeSolverDriver
 {   
     public static void main(String args[]) throws Exception
     {
